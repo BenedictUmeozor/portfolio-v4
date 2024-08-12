0daa26cfcb74f94c75f64052a63b822d0558e2eb
Benedict <benedictumeozor@gmail.com>
2024-08-12 20:19:13 +0100

    changed to react-intersection-observer libray

--- utils/Element.tsx
+++ utils/Element.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React, { useRef, useEffect, useState, ReactNode } from "react";
+import React, { useEffect, ReactNode } from "react";
+import { useInView } from "react-intersection-observer";
 
 export const ElementInView = ({
   children,
@@ -12,46 +13,22 @@ export const ElementInView = ({
   hash: string;
   className?: string;
 }) => {
-  const [inView, setInView] = useState<boolean>(false);
-  const elementRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setInView(true);
-          } else {
-            setInView(false);
-          }
-        });
-      },
-      { threshold: 0.1 } // Fire the callback when at least 50% of the target is visible
-    );
-
-    let currentElement = elementRef.current; // Capture the current value of the ref
-    if (currentElement) {
-      observer.observe(currentElement);
-    }
-
-    return () => {
-      if (currentElement) {
-        observer.unobserve(currentElement);
-      }
-    };
-  }, []);
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+  });
 
   useEffect(() => {
     if (inView && window.innerWidth > 768) {
       // window.location.hash = hash;
       router.push("/#" + hash);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inView, hash]);
 
   return (
-    <div id={hash} ref={elementRef} className={className}>
+    <div id={hash} ref={ref} className={className}>
       {children}
     </div>
   );
